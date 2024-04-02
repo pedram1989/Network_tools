d936409d00ed0fc682b6bbc62504a9427776e445
user <user@XII-CASE>
2024-04-02 12:19:48 +0330

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Cafe_IT___fanap_infra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_name = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label_ip = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Indigo;
+            this.label1.Location = new System.Drawing.Point(31, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PC Name :";
+            // 
+            // label_name
+            // 
+            this.label_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_name.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.label_name.Location = new System.Drawing.Point(232, 35);
+            this.label_name.Name = "label_name";
+            this.label_name.Size = new System.Drawing.Size(270, 46);
+            this.label_name.TabIndex = 1;
+            this.label_name.Text = "PC.Name.lable";
+            this.label_name.Click += new System.EventHandler(this.label_name_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Indigo;
+            this.label3.Location = new System.Drawing.Point(31, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(184, 46);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "IP :";
+            // 
+            // label_ip
+            // 
+            this.label_ip.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_ip.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.label_ip.Location = new System.Drawing.Point(232, 81);
+            this.label_ip.Name = "label_ip";
+            this.label_ip.Size = new System.Drawing.Size(270, 46);
+            this.label_ip.TabIndex = 3;
+            this.label_ip.Text = "IP.Address.lable";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 161);
+            this.Controls.Add(this.label_ip);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label_name);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Pedram Ghanbari";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label_ip;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Cafe_IT___fanap_infra
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
